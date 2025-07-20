f8e6d186efd5f8c01f4e9ab993383237ab29fc0b
chelizichen <1347290221@qq.com>
2025-07-20 11:01:55 +0800

    处理打包问题

--- test/bunserver/build.sh
+++ test/bunserver/build.sh
@@ -1,13 +1,23 @@
-rm ./TestBunServer.tar.gz
+rm ./BunSSRTestServer.tar.gz
 
 rm ./app
+rm -rf ./dist
 # --target=bun-linux-x64
-bun build --compile --minify --sourcemap  ./index.ts --outfile app
-# bun build --compile --minify --sourcemap  --target=bun-linux-x64 ./index.ts --outfile app
+# bun build --compile --minify --sourcemap  ./index.ts --outfile app
+bun build --compile --minify --sourcemap  --target=bun-linux-x64 ./index.ts --outfile app
 
+mkdir -p ./dist/src/client
+cp ./app ./dist/app
+cp ./src/client.tsx ./dist/src/client.tsx
+cp -r ./src/client/* ./dist/src/client/
+cp ./package.json ./dist/package.json
+cp ./sgridnext.release ./dist/sgridnext.release
 
-# tar -czvf TestBunServer.tar.gz ./app ./src/client.tsx
+cd ./dist
+bun i --production
 
-# sgridnext
+tar -czvf BunSSRTestServer.tar.gz ./*
+
+sgridnext
 
 # rm ./app
